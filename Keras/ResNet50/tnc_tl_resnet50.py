15379bf4ec4f4ad833e90aec6901bd25adb95861
Shu Peng <thomasshupeng@hotmail.com>
2018-06-25 23:17:35 -0700

    Keras Transfer Learning - VGG19.

---
+++ Keras/ResNet50/tnc_tl_resnet50.py
@@ -0,0 +1,40 @@
+import keras
+import numpy as np
+
+from keras.applications import resnet50
+#Load the ResNet50 model
+resnet_model = resnet50.ResNet50(weights='imagenet')
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.imagenet_utils import decode_predictions
+import matplotlib.pyplot as plt
+%matplotlib inline
+ 
+filename = 'images/cat.jpg'
+# load an image in PIL format
+original = load_img(filename, target_size=(224, 224))
+print('PIL image size',original.size)
+plt.imshow(original)
+plt.show()
+ '''
+1.Load the image. This is done using the load_img() function. Keras uses the PIL format for loading images. Thus, the image is in width x height x channels format.
+2.Convert the image from PIL format to Numpy format ( height x width x channels ) using image_to_array() function.
+3.The networks accept a 4-dimensional Tensor as an input of the form ( batchsize, height, width, channels). This is done using the expand_dims() function in Numpy.
+
+'''
+# convert the PIL image to a numpy array
+# IN PIL - image is in (width, height, channel)
+# In Numpy - image is in (height, width, channel)
+numpy_image = img_to_array(original)
+plt.imshow(np.uint8(numpy_image))
+plt.show()
+print('numpy array size',numpy_image.shape)
+ 
+# Convert the image / images into batch format
+# expand_dims will add an extra dimension to the data at a particular axis
+# We want the input matrix to the network to be of the form (batchsize, height, width, channels)
+# Thus we add the extra dimension to the axis 0.
+image_batch = np.expand_dims(numpy_image, axis=0)
+print('image batch size', image_batch.shape)
+plt.imshow(np.uint8(image_batch[0]))
