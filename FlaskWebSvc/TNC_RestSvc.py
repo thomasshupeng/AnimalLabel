e18fedff4d74ee67337b697b1aec72d0e3a354a4
Shu Peng <shpeng@microsoft.com>
2018-06-29 00:43:32 -0700

    Draft

---
+++ FlaskWebSvc/TNC_RestSvc.py
@@ -0,0 +1,60 @@
+import os
+import keras
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# 'tncai/v1.0/Prediction/cntk21'
+SERVICE_NAME = 'tncai'
+API_VERSION = 'v1.0'
+END_POINT_NAME = 'Prediction'
+MODEL_NAME = 'cntk21'
+
+
+CNTK_MODEL_FILE = 'TNC_ResNet18_ImageNet_CNTK.model'
+tl_model_file = os.path.join(os.getcwd(), "Model", CNTK_MODEL_FILE)
+# define base model location and characteristics
+_image_height = 682
+_image_width = 512
+_num_channels = 3
+
+# Evaluates a single image using the provided model
+def eval_single_image(loaded_model, image_path, image_width, image_height):
+    # load and format image (resize, RGB -> BGR, CHW -> HWC)
+    img = Image.open(image_path)
+    if image_path.endswith("png"):
+        temp = Image.new("RGB", img.size, (255, 255, 255))
+        temp.paste(img, img)
+        img = temp
+    resized = img.resize((image_width, image_height), Image.ANTIALIAS)
+    bgr_image = np.asarray(resized, dtype=np.float32)[..., [2, 1, 0]]
+    hwc_format = np.ascontiguousarray(np.rollaxis(bgr_image, 2))
+
+    ## Alternatively: if you want to use opencv-python
+    # cv_img = cv2.imread(image_path)
+    # resized = cv2.resize(cv_img, (image_width, image_height), interpolation=cv2.INTER_NEAREST)
+    # bgr_image = np.asarray(resized, dtype=np.float32)
+    # hwc_format = np.ascontiguousarray(np.rollaxis(bgr_image, 2))
+
+    # compute model output
+    arguments = {loaded_model.arguments[0]: [hwc_format]}
+    output = loaded_model.eval(arguments)
+
+    # return softmax probabilities
+    sm = softmax(output[0])
+    return sm.eval()
+
+
+
+@app.route("/" + SERVICE_NAME + "/" + API_VERSION + "/" + END_POINT_NAME)
+def get_prediction():
+  return "Hello, World!"
+
+app.run()
+def main():
+    
+    app.run()
+    return
+
+if __name__ == '__main__':
+    main()
