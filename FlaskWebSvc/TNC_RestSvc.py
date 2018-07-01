abb59e0074f9f79cacada844d33e592e87078010
Shu Peng <thomasshupeng@hotmail.com>
2018-06-30 23:51:53 -0700

    Implemented PredictImageUrl API.

--- FlaskWebSvc/TNC_RestSvc.py
+++ FlaskWebSvc/TNC_RestSvc.py
@@ -1,22 +1,18 @@
-import os
+"""
+File Name: TNC_RestSvc.py
+v 1.0
 
-import numpy as np
+This program provides the RESTful API for TNC project
+
+6/30/2018
+Shu Peng
+"""
 from flask import Flask, jsonify, request
-from PIL import Image
-
-import cntk.io.transforms as xforms
-from cntk import Constant, Trainer, load_model, placeholder
-from cntk.device import gpu, try_set_default_device
-from cntk.io import ImageDeserializer, MinibatchSource, StreamDef, StreamDefs
-from cntk.layers import Dense
-from cntk.learners import (learning_parameter_schedule, momentum_schedule,
-                           momentum_sgd)
-from cntk.logging import ProgressPrinter, log_number_of_parameters
-from cntk.logging.graph import find_by_name, get_node_outputs
-from cntk.losses import cross_entropy_with_softmax
-from cntk.metrics import classification_error
-from cntk.ops import combine, softmax
-from cntk.ops.functions import CloneMethod
+import os
+import requests
+import datetime
+import shutil
+import TNC_ModelLoader
 
 '''
 1.	PredictImageUrl
@@ -24,59 +20,31 @@ https://southcentralus.dev.cognitive.microsoft.com/docs/services/57982f59b5964e3
                 
 This one is predicting image by a given a url, which is very similar to what you have with some changes:
 a.	Instead pass pic URL as url parameter, it passes URL in http body, which doesn’t need any encoding.  
-b.	We can make projectid, iterationid, applicationid all as optional, those id can be useful for different projects(云南老君山，北大生命科学院， etc.)
+b.	We can make projectid, iterationid, applicationid all as optional, those id can be useful for different 
+projects(云南老君山，北大生命科学院， etc.)
 
 2.	PredictImage
 https://southcentralus.dev.cognitive.microsoft.com/docs/services/57982f59b5964e36841e22dfbfe78fc1/operations/5a3044f608fa5e06b890f164
 
 '''
 
-
-
-CNTK_MODEL_FILE = 'TNC_ResNet18_ImageNet_CNTK.model'
-tl_model_file = os.path.join(os.getcwd(), "Model", CNTK_MODEL_FILE)
-
-# define base model location and characteristics
-_image_height = 512
-_image_width = 682
-_num_channels = 3
-
-# Evaluates a single image using the provided model
-def eval_single_image(loaded_model, image_path, image_width, image_height):
-    # load and format image (resize, RGB -> BGR, CHW -> HWC)
-    img = Image.open(image_path)
-    if image_path.endswith("png"):
-        temp = Image.new("RGB", img.size, (255, 255, 255))
-        temp.paste(img, img)
-        img = temp
-    resized = img.resize((image_width, image_height), Image.ANTIALIAS)
-    bgr_image = np.asarray(resized, dtype=np.float32)[..., [2, 1, 0]]
-    hwc_format = np.ascontiguousarray(np.rollaxis(bgr_image, 2))
-
-    ## Alternatively: if you want to use opencv-python
-    # cv_img = cv2.imread(image_path)
-    # resized = cv2.resize(cv_img, (image_width, image_height), interpolation=cv2.INTER_NEAREST)
-    # bgr_image = np.asarray(resized, dtype=np.float32)
-    # hwc_format = np.ascontiguousarray(np.rollaxis(bgr_image, 2))
-
-    # compute model output
-    arguments = {loaded_model.arguments[0]: [hwc_format]}
-    output = loaded_model.eval(arguments)
-
-    # return softmax probabilities
-    sm = softmax(output[0])
-    return sm.eval()
-
-
 project_name_to_id = {'TNC': '11111111',
                       '云南老君山': '22222222',
                       '北大生命科学院': '33333333'}
 
-# 'tncapi/v1.0/Prediction/cntk21'
 SERVICE_NAME = 'tncapi'
 API_VERSION = 'v1.0'
 END_POINT_NAME = 'Prediction'
-MODEL_NAME = 'cntk21'
+MODEL_NAME = '21CResNet18'
+
+# Clean up temp image folder
+temp_folder = os.path.join(os.getcwd(), 'temp')
+if os.path.exists(temp_folder):
+    shutil.rmtree(temp_folder)
+    os.makedirs(temp_folder)
+
+loader = TNC_ModelLoader.ModelLoader()
+model = loader.get_model(MODEL_NAME)
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -85,11 +53,12 @@ app.config["DEBUG"] = True
 endpoint = "/" + SERVICE_NAME + "/" + API_VERSION + "/" + END_POINT_NAME + "/" + project_name_to_id['TNC'] + "/url"
 print("end point1 = ", endpoint)
 
+
 @app.route(endpoint, methods=['POST'])
 def get_prediction_img_url():
     print("=== Arguments ===")
-    iterationId = request.args.get('iterationId')
-    print("iterationId = {!s}".format(iterationId))
+    iteration_id = request.args.get('iterationId')
+    print("iteration_id = {!s}".format(iteration_id))
     application = request.args.get('application')
     print("application = {!s}".format(application))
     print("=== Headers ===")
@@ -101,20 +70,32 @@ def get_prediction_img_url():
     img_url = request.json.get('Url')
     print("Url = {!s}".format(img_url))
 
+    # Download image file
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+    filename = img_url[img_url.rfind('/')+1:]
+    img_path_file = os.path.join(temp_folder, filename)
+
+    r = requests.get(img_url, allow_redirects=True, verify=False, auth=('user', 'pass'))
+    if r.status_code == requests.codes.ok:
+        with open(img_path_file, 'wb') as f:
+            f.write(r.content)
+    else:
+        return jsonify({"Url": img_url, "Code": r.status_code, "Error": r.reason})
+
+    if not os.path.exists(img_path_file):
+        res_error = {"Url": img_url, "Error": "Can not open image file."}
+        return jsonify(res_error)
+
+    predictions = model.predict(img_path_file)
+    os.remove(img_path_file)
+
     res_prediction_img_url = {
         "Id": "string",
         "Project": project_name_to_id['TNC'],
-        "Iteration": iterationId,
-        "Created": "string",
-        "Predictions": [
-            {
-                "TagId": "99",
-                "Tag": "Monkey",
-                "Probability": 0.9999
-            }
-        ]
-    }
-
+        "Iteration": iteration_id,
+        "Created": datetime.datetime.now().isoformat(),
+        "Predictions": predictions}
     return jsonify(res_prediction_img_url)
 
 
@@ -124,4 +105,3 @@ def page_not_found(e):
 
 
 app.run()
-
--- FlaskWebSvc/test/Test_PredictImgUrl.py
+++ FlaskWebSvc/test/Test_PredictImgUrl.py
@@ -1,4 +1,5 @@
-import http.client, urllib.request, urllib.parse, urllib.error, base64
+import http.client
+import urllib.request, urllib.parse, urllib.error
 import json
 
 headers = {
@@ -14,20 +15,18 @@ params = urllib.parse.urlencode({
 })
 
 body = {"Url": "https://theartmad.com/wp-content/uploads/2015/02/Cute-Baby-Monkeys-3.jpg"}
+
 json_body = json.dumps(body)
 try:
-    # conn = http.client.HTTPSConnection('southcentralus.api.cognitive.microsoft.com')
-    # conn.request("POST", "/customvision/v1.1/Prediction/{projectId}/url?%s" % params, "{body}", headers)
     conn = http.client.HTTPConnection('127.0.0.1', port=5000)
     conn.request("POST", "/tncapi/v1.0/Prediction/11111111/url?{!s}".format(params), json_body, headers)
     response = conn.getresponse()
     data = response.read()
-    print(data)
     json_response = json.loads(data)
     predictions = json_response.get('Predictions')
-    if len(predictions)>=1:
+    if len(predictions) >= 1:
         for pred in predictions:
-            print("I'm {:f} sure this is a {!s}!".format(pred.get('Probability'), pred.get('Tag')))
+            print("I'm [{!s}] sure this is a {!s}.".format(pred.get('Probability'), pred.get('Tag')))
     conn.close()
 except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    print("[Error {0}] {1}".format(e.errno, e.strerror))
